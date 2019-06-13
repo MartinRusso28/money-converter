43b08ac2a4b5554f1e9546999416bb213f24e207
Martin.Russo <martin_russo28@hotmail.com>
2019-06-12 22:02:22 -0300

    add test to money adder

--- pkg/adder/money_adder_test.go
+++ pkg/adder/money_adder_test.go
@@ -5,9 +5,10 @@ import(
 	"money-converter/pkg/converter"
 	"money-converter/pkg/converter/model"
 	"github.com/stretchr/testify/assert"
+	"money-converter/test"
 )
 
-func Test_can_add_with_valid_money(t *testing.T) {
+func Test_api_can_add_with_valid_money(t *testing.T) {
 
 	japanese := model.Currency{
 		CurrencyCode:"JPY",
@@ -46,6 +47,57 @@ func Test_can_add_with_valid_money(t *testing.T) {
 	moneyAdder.AddMoney(money1)
 	moneyAdder.AddMoney(money2)
 	moneyAdder.AddMoney(money3)
+
+	result, err := moneyAdder.CalculateSumResult()
+
+	assert.Greater(t, result, float64(0))
+	assert.NoError(t, err)
+}
+
+func Test_repo_can_add_with_valid_money(t *testing.T) {
+
+	repo := mock.GetRepository()
+
+	pesoC := model.Currency{
+		CurrencyCode: "ARS",
+		Rate: 10,
+	}
+
+	dolarC := model.Currency{
+		CurrencyCode: "USD",
+		Rate: 40,
+	}
+
+	err := repo.Money.AddCurrency(pesoC)
+	err = repo.Money.AddCurrency(dolarC)
+
+	moneyAdder := MoneyAdder{
+		CurrencyConverter: moneyconverter.CurrencyConverter{
+			RateStrategy: moneyconverter.RepositoryRateStrategy{Repo: repo},
+			ToCurrency: dolarC,
+		},
+	}
+
+	dolar := model.Currency{
+		CurrencyCode:"USD",
+	}
+
+	peso := model.Currency{
+		CurrencyCode:"ARS",
+	}
+
+	money1 := model.Money{
+		Amount: 10,
+		Currency: dolar,
+	}
+
+	money2 := model.Money{
+		Amount: 100,
+		Currency: peso,
+	}
+
+	moneyAdder.AddMoney(money1)
+	moneyAdder.AddMoney(money2)
 
 	result, err := moneyAdder.CalculateSumResult()
 
